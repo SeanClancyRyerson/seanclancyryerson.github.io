addb1fa3cef1436d266bfa741a99b628dea25aab
Sean Ryerson <seanclancyryerson@gmail.com>
2023-04-09 19:27:35 -0400

    Add nice stlying for checkboxes

--- src/App.js
+++ src/App.js
@@ -92,77 +92,85 @@ function App() {
         <img className="logo-icon" src="./logo512.png" />
       </div>
       <div className="filter-section">
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="brandy"
             type="checkbox"
             checked={spiritBrandy}
             onChange={toggleBrandy}
           ></input>
-          <label htmlFor="brandy">Brandy</label>
+          <label htmlFor="brandy" className="check-box"></label>
+          <span>Brandy</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="gin"
             type="checkbox"
             checked={spiritGin}
             onChange={toggleGin}
           ></input>
-          <label htmlFor="gin">Gin</label>
+          <label htmlFor="gin" className="check-box"></label>
+          <span>Gin</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="mezcal"
             type="checkbox"
             checked={spiritMezcal}
             onChange={toggleMezcal}
           ></input>
-          <label htmlFor="mezcal">Mezcal</label>
+          <label htmlFor="mezcal" className="check-box"></label>
+          <span>Mezcal</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="rum"
             type="checkbox"
             checked={spiritRum}
             onChange={toggleRum}
           ></input>
-          <label htmlFor="rum">Rum</label>
+          <label htmlFor="rum" className="check-box"></label>
+          <span>Rum</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="tequila"
             type="checkbox"
             checked={spiritTequila}
             onChange={toggleTequila}
           ></input>
-          <label htmlFor="tequila">Tequila</label>
+          <label htmlFor="tequila" className="check-box"></label>
+          <span>Tequila</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="vodka"
             type="checkbox"
             checked={spiritVodka}
             onChange={toggleVodka}
           ></input>
-          <label htmlFor="vodka">Vodka</label>
+          <label htmlFor="vodka" className="check-box"></label>
+          <span>Vodka</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="whiskey"
             type="checkbox"
             checked={spiritWhiskey}
             onChange={toggleWhiskey}
           ></input>
-          <label htmlFor="whiskey">Whiskey</label>
+          <label htmlFor="whiskey" className="check-box"></label>
+          <span>Whiskey</span>
         </div>
-        <div className="input-container">
+        <div className="checkbox-wrapper">
           <input
             id="wine"
             type="checkbox"
             checked={spiritWine}
             onChange={toggleWine}
           ></input>
-          <label htmlFor="wine">Wine</label>
+          <label htmlFor="wine" className="check-box"></label>
+          <span>Wine</span>
         </div>
       </div>
       <Cocktail drinks={filteredDrinks} />
