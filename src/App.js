a1be3e62294983c46ae4f61f0f7669f8365bfc38
Sean Ryerson <seanclancyryerson@gmail.com>
2023-04-09 13:35:59 -0400

    Add new drinks to list and add flex-grow to improve layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Cocktail from "./components/Cocktail/Cocktail";
+import Cocktail from "./components/cocktail/Cocktail";
 
 function App() {
   return (
